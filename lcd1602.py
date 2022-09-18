21009a27b8bd7d3fb20434e2001793fd56f20b1b
Pzqqt <821026875@qq.com>
2022-09-18 16:55:12 +0800

    Add files
    Some code references from:
    https://www.basemu.com/16x2-lcd-module-control-using-python-and-rpi.html

---
+++ lcd1602.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python3
+
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    raise ImportError("This doesn't appear to be a Raspberry Pi device!")
+import time
+
+
+# Define GPIO to LCD mapping
+LCD_RS = 16
+LCD_E  = 12
+LCD_D4 = 25
+LCD_D5 = 24
+LCD_D6 = 23
+LCD_D7 = 18
+
+# Define some device constants
+LCD_WIDTH = 16  # Maximum characters per line
+LCD_DAT = True
+LCD_CMD = False
+
+LCD_LINE_1 = 0x80  # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0  # LCD RAM address for the 2nd line
+
+# Timing constants
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+
+def lcd_toggle_enable():
+    # Toggle enable
+    time.sleep(E_DELAY)
+    GPIO.output(LCD_E, True)
+    time.sleep(E_PULSE)
+    GPIO.output(LCD_E, False)
+    time.sleep(E_DELAY)
+
+def lcd_byte(bits, mode):
+    # Send byte to data pins
+    # bits = data
+    # mode = True    for character
+    #                False for command
+
+    GPIO.output(LCD_RS, mode)  # RS
+
+    # High bits
+    GPIO.output(LCD_D4, bits & 0x10 == 0x10)
+    GPIO.output(LCD_D5, bits & 0x20 == 0x20)
+    GPIO.output(LCD_D6, bits & 0x40 == 0x40)
+    GPIO.output(LCD_D7, bits & 0x80 == 0x80)
+
+    # Toggle 'Enable' pin
+    lcd_toggle_enable()
+
+    # Low bits
+    GPIO.output(LCD_D4, bits & 0x01 == 0x01)
+    GPIO.output(LCD_D5, bits & 0x02 == 0x02)
+    GPIO.output(LCD_D6, bits & 0x04 == 0x04)
+    GPIO.output(LCD_D7, bits & 0x08 == 0x08)
+
+    # Toggle 'Enable' pin
+    lcd_toggle_enable()
+
+def lcd_init():
+    # Main program block
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)        # Use BCM GPIO numbers
+    GPIO.setup(LCD_E,  GPIO.OUT)  # E
+    GPIO.setup(LCD_RS, GPIO.OUT)  # RS
+    GPIO.setup(LCD_D4, GPIO.OUT)  # DB4
+    GPIO.setup(LCD_D5, GPIO.OUT)  # DB5
+    GPIO.setup(LCD_D6, GPIO.OUT)  # DB6
+    GPIO.setup(LCD_D7, GPIO.OUT)  # DB7
+
+    # Initialise display
+    lcd_byte(0x33, LCD_CMD)  # 110011 Initialise
+    lcd_byte(0x32, LCD_CMD)  # 110010 Initialise
+    lcd_byte(0x06, LCD_CMD)  # 000110 Cursor move direction
+    lcd_byte(0x0C, LCD_CMD)  # 001100 Display On, Cursor Off, Blink Off
+    lcd_byte(0x28, LCD_CMD)  # 101000 Data length, number of lines, font size
+    lcd_byte(0x01, LCD_CMD)  # 000001 Clear display
+    time.sleep(E_DELAY)
+
+def lcd_string(message: str, line, scrolling=False):
+    # Send string to display
+
+    def _lcd_string(_message: str):
+        lcd_byte(line, LCD_CMD)
+        for j in range(LCD_WIDTH):
+            lcd_byte(ord(_message[j]), LCD_DAT)
+
+    message_len = len(message)
+    if message_len > LCD_WIDTH and scrolling:
+        # 字符串长度大于LCD_WIDTH, 且scrolling为True时, 在同一行"滚动"显示.
+        # 注意此时整个程序是阻塞的.
+        # 这意味着, 如果你的程序是先显示第一行字符串再显示第二行字符串, 且第一行字符串长度大于LCD_WIDTH,
+        # 那么只有第一行字符串滚动完毕了才会开始显示第二行字符串.
+        # 此时你应该让你的程序先显示第二行字符串, 再显示第一行,
+        # 同时也意味着这个函数尚不支持两行同时滚动.
+        for i in range(message_len-LCD_WIDTH+1):
+            _lcd_string(message[i:i+LCD_WIDTH])
+            # 字符串滚动完之后不再sleep
+            if i == message_len - LCD_WIDTH:
+                break
+            if i == 0:
+                time.sleep(1.25)  # 滚动前额外停留1.25秒, 加上下面的0.75秒, 总计停留两秒
+            # 每次滚动停留0.75秒 (停留时间太短的话字符串滚动得太快, 导致字符串难以辨识, 而且很费眼睛)
+            time.sleep(0.75)
+    else:
+        message = message.ljust(LCD_WIDTH, " ")
+        _lcd_string(message)
+
+# https://github.com/T-622/RPI-PICO-I2C-LCD/blob/997b35940bda3addb473dea38c6c10e3cf48855c/lcd_api.py#L153
+def lcd_custom_char(location, charmap):
+    # Write a character to one of the 8 CGRAM locations,
+    # available as chr(0) through chr(7).
+    location &= 0x7
+    lcd_byte(0x40 | (location << 3), LCD_CMD)
+    time.sleep(E_DELAY)
+    for i in range(8):
+        lcd_byte(charmap[i], LCD_DAT)
+        time.sleep(E_DELAY)
+
+def lcd_cleanup():
+    lcd_byte(0x01, LCD_CMD)
+    lcd_string("Goodbye!", LCD_LINE_1)
+    GPIO.cleanup()
---
+++ lcd1602_show.py
@@ -0,0 +1,218 @@
+#!/usr/bin/python3
+
+import time
+import datetime
+import subprocess
+from socket import AF_INET
+try:
+    from gpiozero.pins.data import pi_info
+    from gpiozero.pins.local import get_pi_revision
+except ImportError:
+    raise ImportError("This doesn't appear to be a Raspberry Pi device!")
+
+import psutil
+
+from lcd1602 import (
+    LCD_LINE_1, LCD_LINE_2, lcd_init, lcd_string, lcd_cleanup, lcd_custom_char
+)
+
+
+PI_BOARD_INFO = pi_info()
+BOOT_TIME = psutil.boot_time()
+DATETIME_BOOT_TIME = datetime.datetime(*time.localtime(BOOT_TIME)[:6])
+
+def size_human_readable(int_size):
+    """ 返回人类可读的文件大小 """
+    if int_size < 1024:
+        return "%sB" % (int_size, )
+    if int_size < 1024 * 1024:
+        return "%0.1fK" % (int_size / 1024, )
+    if int_size < 1024 * 1024 * 1024:
+        return "%0.1fM" % (int_size / 1024 / 1024, )
+    return "%0.1fG" % (int_size / 1024 / 1024 / 1024, )
+
+# Unused
+def get_device_model():
+    with open('/sys/firmware/devicetree/base/model', 'r') as f:
+        device_model = f.read().strip()
+        if device_model[-1] == '\x00':
+            device_model = device_model[:-1]
+        return device_model
+
+# Unused
+def get_revision_string():
+    # rc, output = subprocess.getstatusoutput("cat /proc/cpuinfo | grep Revision | cut -d ':' -f2")
+    # if rc != 0:
+    #     return ""
+    # return output.strip()
+    return hex(get_pi_revision())[2:]
+
+def get_cpu_temp():
+    # return psutil.sensors_temperatures()["cpu_thermal"][0].current
+    with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
+        return int(f.read()) / 1000
+
+def get_hostname():
+    with open('/etc/hostname', 'r') as f:
+        return f.read().strip()
+
+def get_kernel_name():
+    with open('/proc/version', 'r') as f:
+        return f.read().strip().split(' ')[2]
+
+def get_bootloader_version():
+    rc, output = subprocess.getstatusoutput('vcgencmd bootloader_version')
+    if rc != 0:
+        return "Unknown"
+    return output.splitlines()[0]
+
+def get_ips():
+    ips_list = []
+    for iface_name, iface_info in psutil.net_if_addrs().items():
+        for item in iface_info:
+            if item.family is AF_INET:
+                iface_addr = item.address
+                break
+        else:
+            iface_addr = "No ip addr"
+        ips_list.append((iface_name, iface_addr))
+    return ips_list
+
+def get_partitions_info():
+    partitions_info = []
+    for partition in psutil.disk_partitions():
+        partition_mountpoint = partition.mountpoint
+        partition_usage = psutil.disk_usage(partition.mountpoint)
+        partitions_info.append(
+            # mount point, total, percent
+            (partition_mountpoint, size_human_readable(partition_usage.total), partition_usage.percent)
+        )
+    return partitions_info
+
+def get_boot_time_delta() -> datetime.timedelta:
+    return datetime.datetime(*time.localtime(time.time())[:6]) - DATETIME_BOOT_TIME
+
+def print_boot_time_delta(timedelta: datetime.timedelta):
+    time_delta_seconds = timedelta.seconds
+    time_delta_days = timedelta.days if timedelta.days <= 999 else 999
+    return "%sd %sh %sm %ss" % (
+        str(time_delta_days).rjust(3),
+        str(int(time_delta_seconds // 60 // 60)).zfill(2),
+        str(int(time_delta_seconds // 60 % 60)).zfill(2),
+        str(int(time_delta_seconds % 60)).zfill(2),
+    )
+
+KERNEL_NAME = get_kernel_name()
+RAM_TOTAL_HUMAN_READABLE = size_human_readable(psutil.virtual_memory().total)
+BOOTLOADER_VERSION = get_bootloader_version()
+
+def main():
+    # Initialise display
+    lcd_init()
+
+    custom_chars = [
+        # bell
+        bytearray([0x04, 0x0e, 0x0e, 0x0e, 0x1f, 0x00, 0x04, 0x00]),
+        # note
+        bytearray([0x02, 0x03, 0x02, 0x0e, 0x1e, 0x0c, 0x00, 0x00]),
+        # clock
+        bytearray([0x00, 0x0e, 0x15, 0x17, 0x11, 0x0e, 0x00, 0x00]),
+        # heart
+        bytearray([0x00, 0x0a, 0x1f, 0x1f, 0x0e, 0x04, 0x00, 0x00]),
+        # duck
+        bytearray([0x00, 0x0c, 0x1d, 0x0f, 0x0f, 0x06, 0x00, 0x00]),
+        # check
+        bytearray([0x00, 0x01, 0x03, 0x16, 0x1c, 0x08, 0x00, 0x00]),
+        # cross
+        bytearray([0x00, 0x1b, 0x0e, 0x04, 0x0e, 0x1b, 0x00, 0x00]),
+        # ret arrow
+        bytearray([0x01, 0x01, 0x05, 0x09, 0x1f, 0x08, 0x04, 0x00]),
+    ]
+    for i, byte_array in enumerate(custom_chars):
+        lcd_custom_char(i, byte_array)
+
+    for i in [[0x10*i, 0x10*(i+1)] for i in range(0x00, 0x0f+1, 2)]:
+        sl_1 = "".join([chr(j) for j in range(i[0], i[0]+16)])
+        sl_2 = "".join([chr(j) for j in range(i[1], i[1]+16)])
+        lcd_string(sl_1, LCD_LINE_1)
+        lcd_string(sl_2, LCD_LINE_2)
+        time.sleep(0.75)
+
+    lcd_string("Raspberry Pi", LCD_LINE_1)
+    lcd_string("%s Rev %s" % (PI_BOARD_INFO.model, PI_BOARD_INFO.pcb_revision), LCD_LINE_2, scrolling=True)
+    time.sleep(3)
+
+    lcd_string("Revision:", LCD_LINE_1)
+    lcd_string(PI_BOARD_INFO.revision, LCD_LINE_2, scrolling=True)
+    time.sleep(3)
+
+    lcd_string("Released:", LCD_LINE_1)
+    lcd_string(PI_BOARD_INFO.released, LCD_LINE_2, scrolling=True)
+    time.sleep(3)
+
+    lcd_string("Manufacturer:", LCD_LINE_1)
+    lcd_string(PI_BOARD_INFO.manufacturer, LCD_LINE_2, scrolling=True)
+    time.sleep(3)
+
+    lcd_string("Bootloader ver:", LCD_LINE_1)
+    lcd_string(BOOTLOADER_VERSION, LCD_LINE_2, scrolling=True)
+    time.sleep(3)
+
+    lcd_string("Kernel:", LCD_LINE_1)
+    lcd_string(KERNEL_NAME, LCD_LINE_2, scrolling=True)
+    time.sleep(3)
+
+    while True:
+        datetime_now = datetime.datetime.now()
+        lcd_string(datetime_now.strftime("%Y-%m-%d %a"), LCD_LINE_1)
+        for _ in range(5):
+            if (datetime_now.hour, datetime_now.minute, datetime_now.second) == (0, 0, 0):
+                lcd_string(datetime_now.strftime("%Y-%m-%d %a"), LCD_LINE_1)
+            lcd_string(datetime_now.strftime(" %I:%M:%S  %p"), LCD_LINE_2)
+            datetime_now += datetime.timedelta(seconds=1)
+            time.sleep(1)
+
+        for _ in range(5):
+            lcd_string("CPU used: %0.1f%%" % psutil.cpu_percent(), LCD_LINE_1)
+            # chr(0xdf) == '°'
+            lcd_string("CPU temp: %0.1f" % get_cpu_temp() + chr(0xdf) + "C", LCD_LINE_2)
+            time.sleep(1)
+
+        lcd_string("RAM total: %s" % RAM_TOTAL_HUMAN_READABLE, LCD_LINE_1)
+        for i in range(3):
+            lcd_string("RAM used: %s" % size_human_readable(psutil.virtual_memory().used), LCD_LINE_2)
+            time.sleep(1)
+        for i in range(3):
+            lcd_string("RAM used: %0.1f%%" % psutil.virtual_memory().percent, LCD_LINE_2)
+            time.sleep(1)
+
+        boot_time_delta = get_boot_time_delta()
+        for i in range(5):
+            lcd_string("Boot time:", LCD_LINE_1)
+            lcd_string(
+                print_boot_time_delta(boot_time_delta + datetime.timedelta(seconds=i)), LCD_LINE_2
+            )
+            time.sleep(1)
+
+        for mount_point, total_size, used_percent in get_partitions_info():
+            lcd_string("Mount: " + mount_point, LCD_LINE_1)
+            lcd_string("Total: " + total_size, LCD_LINE_2)
+            time.sleep(3)
+            lcd_string(" Used: %0.1f%%" % used_percent, LCD_LINE_2)
+            time.sleep(3)
+
+        lcd_string("Hostname:", LCD_LINE_1)
+        lcd_string(get_hostname(), LCD_LINE_2, scrolling=True)
+        time.sleep(3)
+
+        for item in get_ips():
+            iface_name, ip_ = item
+            lcd_string("iface: " + iface_name, LCD_LINE_1)
+            lcd_string(ip_, LCD_LINE_2)
+            time.sleep(3)
+
+if __name__ == '__main__':
+    try:
+        main()
+    finally:
+        lcd_cleanup()
